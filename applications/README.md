635fa2d6dac36208f4c1ea9cb6d6c76615d20e4a
luzhichao1997606 <563451665@qq.com>
2020-10-08 19:13:41 +0800

    提交类型:新增程序 影响范围:应用层 简短描述:完成线程的创建

---
+++ applications/README.md
@@ -0,0 +1,14 @@
+<!--
+ * @fileName: 
+ * @description: 
+ * @version: 
+ * @author: lzc
+ * @date: Do not edit
+ * @lastEditors: lzc
+ * @lastEditTime: Do not Edit
+-->
+# STM32F103 精英版 BSP 功能详解
+
+## 2020年10月8日
+> ### 完成两个动态线程的创建，本次的线程主要是LED的交替闪烁l.
+ 
--- applications/main.c
+++ applications/main.c
@@ -12,23 +12,92 @@
 #include <rtdevice.h>
 #include <board.h>
 
+static rt_thread_t led0_task = RT_NULL;
+static rt_thread_t led1_task = RT_NULL;
 /* defined the LED0 pin: PF7 */
 #define LED0_PIN    GET_PIN(B, 5)
+#define LED1_PIN    GET_PIN(E, 5)
 
-int main(void)
-{
-    int count = 1;
-    /* set LED0 pin mode to output */
+/**
+ * @name: board_Init
+ * @brief: 初始化
+ * @author: lzc
+ * @param {type} None
+ * @return {type} None
+ * @note: 修改记录：初次创建
+ */
+void board_Init (void)
+{ 
+     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-	
+    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+}
 
-    while (count++)
+/**
+ * @name: led0_thread_entry
+ * @brief: LED0的线程函数
+ * @author: lzc
+ * @param {type} None
+ * @return {type} None
+ * @note: 修改记录：初次创建
+ */
+static void led0_thread_entry(void* paremeter)
+{
+    
+    while(1)
     {
         rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
+        rt_thread_mdelay(1500);
         rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
-    }
+        rt_thread_mdelay(1500);   
+    } 
+}
 
-    return RT_EOK;
+/**
+ * @name: led1_thread_entry
+ * @brief: LED1的线程函数
+ * @author: lzc
+ * @param {type} None
+ * @return {type} None
+ * @note: 修改记录：初次创建
+ */
+static void led1_thread_entry(void* paremeter)
+{
+    
+    while(1)
+    {
+        rt_pin_write(LED1_PIN, PIN_HIGH);
+        rt_thread_mdelay(1500);
+        rt_pin_write(LED1_PIN, PIN_LOW);
+        rt_thread_mdelay(500);   
+    } 
+}
+
+/**
+ * @name: main  
+ * @brief: 主函数
+ * @author: lzc
+ * @param {type} None
+ * @return {type} None
+ * @note: 修改记录：初次创建
+ */
+int main(void)
+{ 
+	board_Init();
+    /****************创建动态线程函数************/
+    led1_task = rt_thread_create( "led1",     /*线程名字*/                    
+                                    led1_thread_entry,/*线程入口函数*/
+                                    RT_NULL,/*线程入口函数参数*/
+                                    512,    /*线程栈大小*/
+                                    3 ,    /*线程优先级*/
+                                    20);   /*线程时间片*/ 
+
+    led0_task = rt_thread_create( "led0",     /*线程名字*/                    
+                                    led0_thread_entry,/*线程入口函数*/
+                                    RT_NULL,/*线程入口函数参数*/
+                                    512,    /*线程栈大小*/
+                                    3 ,    /*线程优先级*/
+                                    20);   /*线程时间片*/               
+    rt_thread_startup (led1_task);
+    rt_thread_startup (led0_task);
 }
